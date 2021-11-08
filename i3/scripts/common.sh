5de2de507b4972f49ece072bc3c349e161b8f71d
Bator Horvath <5924171+bhorvath@users.noreply.github.com>
2021-11-08 10:41:53 +1000

    Enable touchpad tap to click

--- i3/scripts/common.sh
+++ i3/scripts/common.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 xset r rate 200 25
-xinput set-prop 12 "libinput Tapping Enabled" 1
+touchpad_id=`xinput | grep Touchpad | sed -E 's/.*id=([[:digit:]]+).*/\1/'`
+xinput set-prop $touchpad_id "libinput Tapping Enabled" 1
