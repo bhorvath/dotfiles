ab80a304dcb198e177e9c09f909ef4eb59c0db9f
Bator Horvath <5924171+bhorvath@users.noreply.github.com>
2021-11-08 10:43:39 +1000

    Update environment scripts

--- i3/scripts/env_home.sh
+++ i3/scripts/env_home.sh
@@ -1,14 +1,4 @@
 #!/bin/sh
 # Home - 2 monitors + laptop
-#xrandr --output DP1 --mode 1920x1080 --pos 0x0 --output DP3 --mode 1920x1080 --pos 1920x0 --output eDP1 --mode 1920x1080 --pos 3400x1080
-# Home - right monitor + laptop
-xrandr --output DP3 --mode 1920x1080 --pos 0x0 --output eDP1 --mode 1920x1080 --pos 1400x1080
-# Home - left monitor + laptop
-# xrandr --output DP1 --mode 1920x1080 --pos 0x0 --output eDP1 --mode 1920x1080 --pos 1000x1080
-
-# Work
-#xrandr --output DP1-3 --mode 1920x1080 --pos 0x0 --output DP2 --mode 2560x1440 --pos 1920x0 --output eDP1 --mode 1920x1080 --pos 2240x1440
-
-# Portable
-#xrandr --output eDP1 --mode 1920x1080 --pos 0x0
-echo Home environment
+xrandr --output DP1 --mode 1920x1080 --pos 0x0 --output DP3 --mode 1920x1080 --pos 1920x0 --output eDP1 --mode 1920x1080 --pos -1200x1080
+#xrandr --output DP1-1-1 --mode 1920x1080 --pos 0x0 --output DP1-1-2 --mode 1920x1080 --pos 1920x0 --output eDP1 --mode 1920x1080 --pos 3400x1080
--- i3/scripts/env_work.sh
+++ i3/scripts/env_work.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 
 # Work
-xrandr --output DP1-3 --mode 1920x1080 --pos 0x0 --output DP2 --mode 2560x1440 --pos 1920x0 --output eDP1 --mode 1920x1080 --pos 2240x1440
+xrandr --output eDP1 --mode 1920x1080 --pos 2240x1440 --rate 60
+xrandr --output DP1-1 --mode 1920x1080 --pos 0x0 --rate 144
+xrandr --output DP1-2 --mode 2560x1440 --pos 1920x0 --rate 60
