72ce073519b2849a6820ed791f33b5c9d23ba9bc
Bator Horvath <5924171+bhorvath@users.noreply.github.com>
2021-11-24 08:36:15 +1000

    Update home workspaces

--- i3/scripts/env_home.sh
+++ i3/scripts/env_home.sh
@@ -1,4 +1,3 @@
 #!/bin/sh
-# Home - 2 monitors + laptop
-xrandr --output DP1 --mode 1920x1080 --pos 0x0 --output DP3 --mode 1920x1080 --pos 1920x0 --output eDP1 --mode 1920x1080 --pos -1200x1080
-#xrandr --output DP1-1-1 --mode 1920x1080 --pos 0x0 --output DP1-1-2 --mode 1920x1080 --pos 1920x0 --output eDP1 --mode 1920x1080 --pos 3400x1080
+# Home: 1 monitor + laptop
+xrandr --output DP1 --mode 3840x2160 --pos 0x0 --rate 60 --output eDP1 --mode 1920x1080 --pos 3840x1600 --rate 60
