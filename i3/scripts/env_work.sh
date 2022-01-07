7fe728b70ff3140e6daf9450239d829673217a05
Bator Horvath <5924171+bhorvath@users.noreply.github.com>
2022-01-08 08:36:12 +1000

    Update work monitor config

--- i3/scripts/env_work.sh
+++ i3/scripts/env_work.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 # Work
-xrandr --output eDP1 --mode 1920x1080 --pos 2240x1440 --rate 60
 xrandr --output DP1-1 --mode 1920x1080 --pos 0x0 --rate 144
-xrandr --output DP1-2 --mode 2560x1440 --pos 1920x0 --rate 60
+xrandr --output DP1-2 --mode 2560x1440 --pos 1920x-250 --rate 60
+xrandr --output eDP1 --mode 1920x1080 --pos 2240x1440
