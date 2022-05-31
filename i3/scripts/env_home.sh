8e5d1958340af66d6e15700195bf5cdc59793618
Bator Horvath <5924171+bhorvath@users.noreply.github.com>
2022-05-31 15:58:32 +1000

    Modify home monitor layout

--- i3/scripts/env_home.sh
+++ i3/scripts/env_home.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-# Home: 1 monitor + laptop
-xrandr --output DP1 --mode 3840x2160 --pos 0x0 --rate 60 --output eDP1 --mode 1920x1080 --pos 3840x1600 --rate 60
+# Home: 1 monitor
+xrandr --output DP1 --mode 3840x2160 --pos 0x0 --rate 60 --output eDP1 --off
