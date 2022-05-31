39476bac95b85ded424f0711408ebe4901880f91
Bator Horvath <5924171+bhorvath@users.noreply.github.com>
2022-05-31 15:57:16 +1000

    Move keyboard rate to separate script

--- i3/scripts/common.sh
+++ i3/scripts/common.sh
@@ -1,4 +1,3 @@
-#!/bin/sh
-xset r rate 200 25
+#!/usr/bin/env sh
 touchpad_id=`xinput | grep Touchpad | sed -E 's/.*id=([[:digit:]]+).*/\1/'`
 xinput set-prop $touchpad_id "libinput Tapping Enabled" 1
